4eea1c4d8bba021a89c93ab2846112780a847afd
MD. ARIFUL HASAN TOTUL <mdahtotul@gmail.com>
2021-12-27 21:53:37 +0600

    error

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,57 +1,64 @@
+import { Container, Grid } from "@mui/material";
+import { Box } from "@mui/system";
 import React from "react";
 import "./Footer.css";
 
 const Footer = () => {
    return (
-      <section className="container-fluid footer">
-         <div className="footer_wrapper">
-            {/* About us  */}
-            <article className="box">
-               <h3>About us</h3>
-               <p>
-                  We are a full-service best travel agency in Cumilla,
-                  Bangladesh with a dedicated team of highly skilled and
-                  experienced people to assist our valuable clients according to
-                  their goals and desired journeys all over the world. We at
-                  Adventure travels and tours strive our best to deliver the
-                  outstanding service to all our customers.
-               </p>
-            </article>
+      <Container
+         maxWidth="xl"
+         className="fourth_sec"
+         sx={{ background: "var(--dark)" }}
+      >
+         <Box sx={{ flexGrow: 1 }}>
+            <Grid container sx={{ justifyContent: "center" }}>
+               <Grid item xs={11} md={6} lg={3} className="first_div">
+                  <h3>About us</h3>
+                  <p>
+                     We are a full-service best travel agency in Cumilla,
+                     Bangladesh with a dedicated team of highly skilled and
+                     experienced people to assist our valuable clients according
+                     to their goals and desired journeys all over the world. We
+                     at Adventure travels and tours strive our best to deliver
+                     the outstanding service to all our customers.
+                  </p>
+               </Grid>
 
-            <article className="box">
-               <h3>Branch location</h3>
-               <li>India</li>
-               <li>Bangladesh</li>
-               <li>Japan</li>
-               <li>USA</li>
-               <li>France</li>
-            </article>
+               <Grid item xs={11} md={6} lg={3} className="second_div">
+                  <h3>Branch location</h3>
+                  <li>India</li>
+                  <li>Bangladesh</li>
+                  <li>Japan</li>
+                  <li>USA</li>
+                  <li>France</li>
+               </Grid>
 
-            <article className="box">
-               <h3>Quick links</h3>
-               <li>Home</li>
-               <li>Packages</li>
-               <li>Services</li>
-               <li>Gallery</li>
-               <li>Review</li>
-               <li>Contact</li>
-            </article>
+               <Grid item xs={11} md={6} lg={3} className="third_div">
+                  <h3>Quick links</h3>
+                  <li>Home</li>
+                  <li>Packages</li>
+                  <li>Services</li>
+                  <li>Gallery</li>
+                  <li>Review</li>
+                  <li>Contact</li>
+               </Grid>
 
-            <article className="box">
-               <h3>Follow us</h3>
-               <li>Facebook</li>
-               <li>Instagram</li>
-               <li>Twitter</li>
-               <li>Linkedin</li>
-            </article>
-         </div>
+               <Grid item xs={11} md={6} lg={3} className="four_div">
+                  <h3>Follow us</h3>
+                  <li>Facebook</li>
+                  <li>Instagram</li>
+                  <li>Twitter</li>
+                  <li>Linkedin</li>
+               </Grid>
+            </Grid>
+         </Box>
          <article>
             <h1 className="credit">
                &copy; created by <span>md ariful hasan</span> | all right
                reserved.
             </h1>
          </article>
-      </section>
+      </Container>
    );
 };
 
